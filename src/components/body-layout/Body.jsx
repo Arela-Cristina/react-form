ad20259ec72a8576b260d9d615a1976e75207d1c
Arela-Cristina <instagrandine@gmail.com>
2024-12-02 21:28:32 +0100

    feat: use hook to manage a form which submit value

--- src/components/body-layout/Body.jsx
+++ src/components/body-layout/Body.jsx
@@ -1,11 +1,15 @@
 import style from "./body.module.css";
 import Tags from "../Tags/Tag.jsx";
 import Card from "./card/Card.jsx";
+import imgVuota from "../../assets/imagine-vuota.jpg";
 import brawlStars from "../../assets/database/brawlStars.js";
+import {useState} from 'react';
 
 export default function Body() {
+
+  //tag che stampiano sotto il header
   const tagTopics = [];
-  console.log(tagTopics);
+  // console.log(tagTopics);
 
   brawlStars.forEach((el) => {
     // console.log("ecco tutti tipi di  tag", el.tag);
@@ -16,14 +20,69 @@ export default function Body() {
     });
   });
 
+  //state della struttura dati
+  const [ brawler, setBrawler ] = useState(brawlStars);
+
+  //state per input text
+  const [ newBrawler, setNewBrawler ] = useState ('Nita');
+
+
+  // GESTIONE INPUT TEXT
+  //creiamo una funzione per gestire il value di imput Text
+  function onTextChange(e){
+    console.log(e.target.value) //=> acccedere a la proprieta value
+    setNewBrawler(e.target.value) //Passiamo il value alla funzione setNewBrawler del nostro state per  input text
+  }
+
+  
+  //GESTIONE INPUT SUBMIT
+  function addBrawler(e){
+    e.preventDefault()//per evitare dei risettare il sito
+   
+    const newBrawlerObject = {
+      id: brawler.length + 1, // nuovo Id value
+      name: newBrawler, // Nuovo Brawler value
+      description: "This is a new Brawler", // nuova Description value
+      thumb: imgVuota, // Nuova Thumb value
+      tag: ["New"], // Nuova Tag value
+      published: false, //Nuovo Booleano
+    };
+
+    //nel nostro array, creiamo un nuovo array con il valore che ci arriva da Set Brawler
+    setBrawler([...brawler, newBrawlerObject])
+    console.log(setBrawler([...brawler, newBrawlerObject]))
+    setNewBrawler('')//dopo Submit svuotiamo il campo di input text
+  }
+
   return (
     <main>
       <section className={style.tagsContainer}>
         <Tags  tags={tagTopics} />
+
+
+
+        <div>
+         
+          <form action="" onSubmit= { addBrawler }>   {/* //FORM */}
+
+            <input  //INPUT TEXT
+            onChange = { onTextChange }
+            type="text" 
+            placeholder="Aggiungi un Brawler" 
+            value = { newBrawler }/>
+
+            <input  //INPUT SUBMIT 
+            type="submit" 
+            value='Aggiungi'/>
+          </form>
+        </div>
+
+
+
       </section>
       <section className={style.cardContainer}>
         <div className={style.col}>
-          {brawlStars.map((el) => (
+          {brawler.map((el) => (
             <Card
               thumb={el.thumb}
               title={el.name}
