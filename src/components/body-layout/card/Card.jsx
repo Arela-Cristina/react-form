59f0a271cd26ca2b0627e477910ae735ce98205a
Arela-Cristina <instagrandine@gmail.com>
2024-12-02 21:25:45 +0100

    import new component DeletedButton and add style

--- src/components/body-layout/card/Card.jsx
+++ src/components/body-layout/card/Card.jsx
@@ -1,24 +1,28 @@
 import style from "./Card.module.css";
 import Tags from "../../../components/Tags/Tag.jsx";
 import Button from "../ui/Button/Button.jsx";
-import imgVuota from "../../../assets/imagine-vuota.jpg"
+import DeletedButton from "../ui/Button/DeleteButton.jsx";
+import imgVuota from "../../../assets/imagine-vuota.jpg";
 
 
 export default function Card({ thumb, title, description, id, published, tag }) {
 
   return (
+    
     <div className={style.card}>
       <img
         className={style.thumb}
         src={published ? `src/assets/${thumb}` : imgVuota}
         alt="Mandys candies"
       />
+      <DeletedButton />
       <div className={style.cardBody}>
         <h3 className="card-title">{title}</h3>
-          <Tags tags={tag}/>
+        <Tags tags={tag} />
         <p className="card-text">{description}</p>
         <Button />
       </div>
     </div>
+
   );
 }
